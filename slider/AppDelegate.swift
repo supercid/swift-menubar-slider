cc5886c0d895f18c07f7d5542f48e401d207cbfe
Cid Lopes <alannettto@gmail.com>
2019-07-10 17:35:58 +0300

    Initial Commit

---
+++ slider/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  slider
+//
+//  Created by Alcidemar Lopes de Araujo Netto on 10/07/2019.
+//  Copyright © 2019 Alcidemar Lopes de Araujo Netto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ slider/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  slider
+//
+//  Created by Alcidemar Lopes de Araujo Netto on 10/07/2019.
+//  Copyright © 2019 Alcidemar Lopes de Araujo Netto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
