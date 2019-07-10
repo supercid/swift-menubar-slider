24cd316e5bceb586d667720478dbb18b74647f9f
Cid Lopes <alannettto@gmail.com>
2019-07-10 17:46:38 +0300

    Add basic slider example

--- slider/AppDelegate.swift
+++ slider/AppDelegate.swift
@@ -1,26 +1,33 @@
-//
-//  AppDelegate.swift
-//  slider
-//
-//  Created by Alcidemar Lopes de Araujo Netto on 10/07/2019.
-//  Copyright © 2019 Alcidemar Lopes de Araujo Netto. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    @IBOutlet weak var window: NSWindow!
+    let statusItem = NSStatusBar.system.statusItem(withLength: -2)
+    
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        let menu = NSMenu()
+        let menuItem = NSMenuItem()
+        let statusSlider = NSSlider()
+        
+        statusSlider.setFrameSize(NSSize(width: 160, height: 16))
+        
+        menu.addItem(NSMenuItem(title: "Slider:", action: nil, keyEquivalent: ""))
+        
+        menuItem.title = "Slider 1"
+        menuItem.view = statusSlider
+        menu.addItem(menuItem)
+        
+        menu.addItem(NSMenuItem.separator())
+
+        
+        menu.addItem(NSMenuItem(title: "Quit", action: Selector("terminate:"), keyEquivalent: "q"))
+        
+        statusItem.image = NSImage(named: "NSStatusAvailable")
+        statusItem.menu = menu
+
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
